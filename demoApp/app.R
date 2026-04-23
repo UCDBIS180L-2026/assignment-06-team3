e11bef951df1ad7d6738a25df89a937bf932f1dc
NorbayaD <njdurr@ucdavis.edu>
2026-04-23 14:32:05 -0700

    updates

--- demoApp/app.R
+++ demoApp/app.R
@@ -1,45 +1,41 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(tidyverse)
 
 data(iris)
 
+
+
+
+
+
 # Define UI for application that draws a box plot
 ui <- fluidPage( #create the overall page
+  
+  # Application title
+  titlePanel("Iris Data"),
+  
+  # Some helpful information
+  helpText("This application creates a boxplot to show difference between",
+           "iris species.  Please use the radio box below to choose a species",
+           "for plotting"),
+  
+  # Sidebar with a radio box to input which trait will be plotted
+  sidebarLayout(
+    sidebarPanel(
+      radioButtons("species", #the input variable that the value will go into
+                   "Choose a trait to display:",
+                   c("Setosa",
+                     "Versicolor",
+                     "Virginica")
+                   
+      )),
     
-    # Application title
-    titlePanel("Iris Data"),
-    
-    # Some helpful information
-    helpText("This application creates a boxplot to show difference between",
-             "iris species.  Please use the radio box below to choose a trait",
-             "for plotting"),
-    
-    # Sidebar with a radio box to input which trait will be plotted
-    sidebarLayout(
-      sidebarPanel(
-        radioButtons("trait", #the input variable that the value will go into
-                     "Choose a trait to display:",
-                     c("Sepal.Length",
-                       "Sepal.Width",
-                       "Petal.Length",
-                       "Petal.Width")
-        )),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        plotOutput("boxPlot")
-      )
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("boxPlot")
     )
   )
+)
 
 
 # Define server logic required to draw a box. plot
@@ -71,3 +67,7 @@ server <- function(input, output) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
+
+
+
