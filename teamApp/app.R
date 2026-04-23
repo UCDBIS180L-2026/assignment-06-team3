04fcc6b58f3117beed674a9579cdba2916c4e905
Quynh Nguyen <ngtrangquynh1@gmail.com>
2026-04-23 15:20:03 -0700

    setup data

--- teamApp/app.R
+++ teamApp/app.R
@@ -1,9 +1,15 @@
 library(shiny)
 # other libraries here
-
+library(tidyverse)
 # data loading and one-time processing here
+setwd("~/Desktop/Assignment/assignment-06-team3")
+load("data_from_SNP_lab.Rdata")
+pheno.geno.pca.pop <- left_join(geno.pca.pop, data.pheno, by=c("ID" = "ID"))
 
+#get rid of spaces in the phenotype names with "make.names()"
+colnames(pheno.geno.pca.pop) <- make.names(colnames(pheno.geno.pca.pop))
 
+head(pheno.geno.pca.pop)
 # Define UI for application 
 ui <- fluidPage( #create the overall page
     #UI code here
